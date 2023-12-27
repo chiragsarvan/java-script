0479fb8f6152332561c2517c36c70af6bc91663e
chiragsarvan <126157016+chiragsarvan@users.noreply.github.com>
2023-12-27 06:33:29 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+console.log(3  
+    +
+     3);// it is not good code reardabilaty
+
+ let name = "chirag"
+ let age = 23
+ let isLoggIn = false
+ let state = null 
+ 
+//  number => 2 to power of 53  // this is limit
+//  bigint => //no limit
+//  string => ""
+//  boolean => true/false
+//  null => stand alone value // it is type is object
+//  undefined => //no value usind // it is type is undefind 
+//  symbol =>  unique 
+
+
+//  object
+
+console.log(typeof state);
