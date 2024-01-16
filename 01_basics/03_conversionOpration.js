c576c2261dc9f115fada9b25740689f28e1c634c
chiragsarvan <126157016+chiragsarvan@users.noreply.github.com>
2024-01-16 06:10:56 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,32 @@
+let scor = undefined
+
+// console.log(typeof scor);
+// console.log(typeof(scor));
+
+let coavertInNumber = Number(scor)
+// console.log(typeof coavertInNumber);
+// console.log(coavertInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+// null => 0
+// undifind => NaN
+
+let isLoggedIn = undefined
+
+let BollensLogedIn = Boolean(isLoggedIn)
+// console.log(BollensLogedIn);
+
+// 1 => true; 0 =>false
+//"" or null or undifind=> false
+//"chirag" => true
+
+
+let age = 22
+console.log(age);
+
+let stringAge = String(age)
+console.log(stringAge);
+console.log(typeof(stringAge));
